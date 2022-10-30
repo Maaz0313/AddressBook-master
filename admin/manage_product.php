2b7d822269b4778251a4236f6914396409e0be4e
Maaz0313 <maaztajammul63@gmail.com>
2022-10-31 04:31:26 +0500

    Modified

---
+++ admin/manage_product.php
@@ -0,0 +1,173 @@
+<?php
+require('top.inc.php');
+$categories_id='';
+$name='';
+$mrp='';
+$price='';
+$qty='';
+$image='';
+$short_desc='';
+$description='';
+$meta_title='';
+$meta_desc='';
+$meta_keyword='';
+
+$msg='';
+if (isset($_GET['id']) && $_GET['id']!='') {
+    $id = get_safe_value($conn,$_GET['id']);
+    $res = mysqli_query($conn,"SELECT * FROM product where id='$id'");
+    $check=mysqli_num_rows($res);
+    if($check>0)
+    {
+        $row = mysqli_fetch_assoc($res);
+        $categories_id=$row['categories_id'];
+        $name=$row['name'];
+        $mrp=$row['mrp'];
+        $price=$row['price'];
+        $qty=$row['qty'];
+        $short_desc=$row['short_desc'];
+        $description=$row['description'];
+        $meta_title=$row['meta_title'];
+        $meta_desc=$row['meta_desc'];
+        $meta_keyword=$row['meta_keyword'];
+    }
+    else
+    {
+        header('location:product.php');
+        die();
+    }
+    
+}
+if (isset($_POST['submit'])) {
+    $categories_id = get_safe_value($conn,$_POST['categories_id']);
+    $name = get_safe_value($conn,$_POST['name']);
+    $mrp = get_safe_value($conn,$_POST['mrp']);
+    $price = get_safe_value($conn,$_POST['price']); //1:36:26
+    $qty = get_safe_value($conn,$_POST['qty']);
+    $short_desc = get_safe_value($conn,$_POST['short_desc']);
+    $description = get_safe_value($conn,$_POST['description']);
+    $meta_title = get_safe_value($conn,$_POST['meta_title']);
+    $meta_desc = get_safe_value($conn,$_POST['meta_desc']);
+    $meta_keyword = get_safe_value($conn,$_POST['meta_keyword']);
+
+
+    $res = mysqli_query($conn,"SELECT * FROM product where name='$name'");
+    $check=mysqli_num_rows($res);
+    if($check>0)
+    {
+        if (isset($_GET['id']) && $_GET['id']!='') {
+            $getData=mysqli_fetch_assoc($res);
+            if($id==$getData['id'])
+            {
+
+            }
+            else
+            {
+                $msg='Product already exists';
+            }
+        }
+        else
+        {
+            $msg='Product already exists';
+        }
+    }
+
+
+
+    if($msg='')
+    {
+        if (isset($_GET['id']) && $_GET['id']!='') {
+            mysqli_query($conn,"UPDATE product SET categories_id='$categories_id',name='$name',mrp='$mrp',price='$price',qty='$qty',short_desc='$short_desc',description='$description',meta_title='$meta_title',meta_desc='$meta_desc',meta_keyword='$meta_keyword' WHERE id='$id'");
+        }
+        else
+        {
+            $image = rand(1111111,99999999).'_'.$_FILES['image']['name'];
+            move_uploaded_file($_FILES['image']['tmp_name'],'../media/product/'.$image);
+            mysqli_query($conn,"INSERT INTO product(categories_id,name,mrp,price,qty,short_desc,description,meta_title,meta_desc,meta_keyword,status,image) VALUES('$categories_id','$name','$mrp','$price','$qty','$short_desc','$description','$meta_title','$meta_desc','$meta_keyword',1,'$image')");
+        }
+        header('location:product.php');
+        die();
+    }
+}
+
+?>
+<div class="content pb-0">
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                <div class="card-header"><strong>Product</strong><small> Form</small></div>
+                <form method="post" enctype="multipart/form-data">
+                    <div class="card-body card-block">
+                        <div class="form-group">
+                            <label for="categories" class="form-control-label">Categories</label>
+                            <select class="form-control" name="categories_id">
+                                <option>Select Category</option>
+                                <?php
+                                $res=mysqli_query($conn,"SELECT id,categories FROM categories ORDER BY categories ASC");
+                                while($row=mysqli_fetch_assoc($res))
+                                {
+                                    if($row['id']==$categories_id)
+                                    {
+                                    echo "<option selected value=".$row['id'].">".$row['categories']."</option>";
+                                    }
+                                    else
+                                    {
+                                    echo "<option value=".$row['id'].">".$row['categories']."</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="categories" class="form-control-label">Product Name</label>
+                            <input type="text" name="name" placeholder="Enter product name" class="form-control" required value="<?php echo $name ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="mrp" class="form-control-label">MRP</label>
+                            <input type="text" name="mrp" placeholder="Enter product mrp" class="form-control" required value="<?php echo $mrp ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="price" class="form-control-label">Price</label>
+                            <input type="text" name="name" placeholder="Enter product price" class="form-control" required value="<?php echo $price ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="qty" class="form-control-label">Qty </label>
+                            <input type="text" name="qty" placeholder="Enter qty" class="form-control" required value="<?php echo $qty ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="image" class="form-control-label">Image </label>
+                            <input type="file" name="name" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="short_desc" class="form-control-label">Short Description </label>
+                            <textarea name="short_desc" placeholder="Enter product short description" class="form-control" required><?php echo $short_desc ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="description" class="form-control-label">Description </label>
+                            <textarea name="description" placeholder="Enter product description" class="form-control" required><?php echo $description ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="meta_title" class="form-control-label">Meta Title </label>
+                            <textarea name="meta_title" placeholder="Enter product meta title" class="form-control"><?php echo $meta_title ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="meta_desc" class="form-control-label">Meta Description </label>
+                            <textarea name="meta_desc" placeholder="Enter product meta description" class="form-control"><?php echo $meta_desc ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="meta_keyword" class="form-control-label">Meta Keyword</label>
+                            <textarea name="meta_keyword" placeholder="Enter meta keyword" class="form-control" ><?php echo $meta_keyword ?></textarea>
+                        </div>
+                        <button id="payment-button" name="submit" type="submit" class="btn btn-lg btn-info btn-block">
+                        <span id="payment-button-amount">Submit</span>
+                        </button>
+                    <div class="field_error"><?php echo $msg?></div>
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require('footer.inc.php') ?>
--- admin/top.inc.php
+++ admin/top.inc.php
@@ -49,7 +49,7 @@ else
                      <a href="#" > User Master</a>
                   </li>
                   <li class="menu-item-has-children dropdown">
-                     <a href="#" > Contact Us</a>
+                     <a href="contact_us.php" > Contact Us</a>
                   </li>
                </ul>
             </div>
