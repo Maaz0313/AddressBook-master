92a8214fc77256e4fca40dc5872d2574ffc0d3d1
Maaz0313 <maaztajammul63@gmail.com>
2022-11-09 11:21:55 +0500

    Created

--- js/main.js
+++ js/main.js
@@ -426,11 +426,105 @@
         });
     }
 
-
-
-
 })(jQuery);
 
+function send_message(){
+    var name=jQuery("#name").val();
+    var email=jQuery("#email").val();
+    var mobile=jQuery("#mobile").val();
+    var message=jQuery("#message").val();
 
+    if(name==""){
+        alert('please enter name');
+    }else if(email==""){
+        alert('please enter email');
+    }else if(mobile==""){
+        alert('please enter mobile');
+    }else if(message==""){
+        alert('please enter message');
+    }else{
+        jQuery.ajax({
+            url:'send_message.php',
+            type:'post',
+            data:'name='+name+'&email='+email+'&mobile='+mobile+'&message='+message,
+            success:function(result){
+                alert(result);
+            }
+        });
+    }
+}
 
+function user_register(){
+    jQueryO('.field_error').html('');
+    var name=jQuery("#name").val();
+    var email=jQuery("#email").val();
+    var mobile=jQuery("#mobile").val();
+    var password=jQuery("#password").val();
 
+    if(name==""){
+        jQuery('#name_error').html('please enter name');
+        is_error='yes';
+    }if(email==""){
+        jQuery('#email_error').html('please enter email');
+        is_error='yes';
+    }if(mobile==""){
+        jQuery('#mobile_error').html('please enter mobile');
+        is_error='yes';
+    }if(password==""){
+        jQuery('#password_error').html('please enter password');
+        is_error='yes';
+    }
+    if(is_error==''){
+        jQuery.ajax({
+            url:'register_submit.php',
+            type:'post',
+            data:'name='+name+'&email='+email+'&mobile='+mobile+'&password='+password,
+            success:function(result){
+                if(result=='email_present'){
+                    jQuery('#email_error').html('Email id already present');
+                }if(result=='insert'){
+                    jQuery('#register_msg p').html('successfully');
+                }
+            }
+        });
+    }
+}
+
+function user_login(){
+    jQueryO('.field_error').html('');
+    var email=jQuery("#login_email").val();
+    var password=jQuery("#login_password").val();
+    var is_error='';
+    }if(email==""){
+        jQuery('#login_email_error').html('please enter email');
+        is_error='yes';
+    }if(password==""){
+        jQuery('#login_password_error').html('please enter password');
+        is_error='yes';
+    }
+    if(is_error==''){
+        jQuery.ajax({
+            url:'login_submit.php',
+            type:'post',
+            data:'email='+email+'&password='+password,
+            success:function(result){
+                if(result=='wrong'){
+                    jQuery('.login_msg p').html('Please enter valid login details');
+                }if(result=='valid'){
+                    window.location.href='index.php';
+                }
+            }
+        });
+    }
+function manage_cart(pid,type){
+    var email=jQuery("#qty").val();
+    jQuery.ajax({
+        url:'manage_cart.php',
+        type:'post',
+        data:'pid='+pid+'&qty='+qty+'&type='+type,
+        success:function(result){
+            jQuery('.htc__qua').html(result);
+        }
+    });
+}
+    
---
+++ send_message.php
@@ -0,0 +1,12 @@
+<?php
+require('connection.inc.php');
+require('functions.inc.php');
+
+$name=get_safe_value($conn,$POST['name']);
+$email=get_safe_value($conn,$POST['email']);
+$mobile=get_safe_value($conn,$POST['mobile']);
+$comment=get_safe_value($conn,$POST['message']);
+$added_on=date('y-m-d h:i:s');
+mysqli_query($conn,"insert into contact_us(name,email,mobile,comment,add_on) value('$name','$email','$mobile','$comment','$added_on')" );
+echo "Thank you";
+?>
