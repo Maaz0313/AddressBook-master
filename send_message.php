fb724c6ec25aa481cd6a65fbbf339105e7324e52
Maaz0313 <maaztajammul63@gmail.com>
2022-11-13 11:28:42 +0500

    Modified

--- send_message.php
+++ send_message.php
@@ -2,11 +2,11 @@
 require('conn.inc.php');
 require('functions.inc.php');
 
-$name=get_safe_value($conn,$POST['name']);
-$email=get_safe_value($conn,$POST['email']);
-$mobile=get_safe_value($conn,$POST['mobile']);
-$comment=get_safe_value($conn,$POST['message']);
+$name=get_safe_value($conn,$_POST['name']);
+$email=get_safe_value($conn,$_POST['email']);
+$mobile=get_safe_value($conn,$_POST['mobile']);
+$comment=get_safe_value($conn,$_POST['message']);
 $added_on=date('y-m-d h:i:s');
-mysqli_query($conn,"insert into contact_us(name,email,mobile,comment,add_on) value('$name','$email','$mobile','$comment','$added_on')" );
+mysqli_query($conn,"insert into contact_us(name,email,mobile,comment,added_on) value('$name','$email','$mobile','$comment','$added_on')" );
 echo "Thank you";
 ?>
