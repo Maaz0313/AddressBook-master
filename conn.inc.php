9220b56335b9b3fba2c09a98a5336f1113d712c7
Maaz0313 <maaztajammul63@gmail.com>
2022-11-17 15:40:49 +0500

    added href in cart.php

--- conn.inc.php
+++ conn.inc.php
@@ -2,7 +2,7 @@
 session_start();
 $conn = mysqli_connect("localhost","root","","addressbook");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/AddressBook-master');
-define('SITE_PATH','http://127.0.0.1/AddressBook-master');
+define('SITE_PATH','http://127.0.0.1/AddressBook-master/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'/media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'/media/product/');
