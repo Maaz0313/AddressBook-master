be91603bec702a04aab41ae980b981bdc97903cb
huzaifa412005 <109646162+huzaifa412005@users.noreply.github.com>
2022-11-22 12:37:34 +0500

    M

--- cart.php
+++ cart.php
@@ -59,9 +59,9 @@ require('top.php');
                                             </td>
                                             <td class="product-price"><span class="amount">Rs. <?php echo $price?></span></td>
                                             <td class="product-quantity"><input type="number" id="<?php echo $key?>qty" value="<?php echo $qty?>" />
-                                            <br><a href="javascript:void(0)" onclick="manage_cart('<?php $key?>','update')">update</a></td>
+                                            <br><a href="javascript:void(0)" onclick="manage_cart('<?php echo $key?>','update')">update</a></td>
                                             <td class="product-subtotal"><?php echo $qty*$price?></td>
-<td class="product-remove"><a href="javascript:void(0)" onclick="manage_cart('<?php $key?>','remove')"><i class="icon-trash icons"></i></a></td>
+<td class="product-remove"><a href="javascript:void(0)" onclick="manage_cart('<?php echo $key?>','remove')"><i class="icon-trash icons"></i></a></td>
                                         </tr>
                                         <?php }?>
                                     </tbody>
